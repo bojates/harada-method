991494ca82c29c0e9916f219aeb0c442448bf7f2
Jemima Kingsley <jemima@bojates.com>
2024-01-12 16:05:21 +0000

    Basic layout

--- app/qualities/page.js
+++ app/qualities/page.js
@@ -1,3 +1,4 @@
+'use client'
 const qualities = [
     'Courtesy',
     'Integrity',
@@ -7,11 +8,27 @@ const qualities = [
 ];
 
 export default function Page() {
+    let value = '';
+    function handleQuality(e) {
+        // console.log('changed');
+        // console.log(e.target);
+        // const form = e.target;
+        // const formData = new FormData(form);
+        value = e.target.value;
+        // console.log(e.target.value)
+        // alert('changed');
+    }
     const qualityList =  qualities.map(quality => {
         return (
-        <div key={quality}>
-            <label for={quality}>{quality}</label>
-            <input type="range" max={10} min={1} id={quality} list="values" name={quality} />
+        <div key={quality} className="p-2">
+            <div>
+            <label for={quality} className="text-xl text-pink-900 font-semibold block">{quality}</label>
+            <input onChange={handleQuality} type="range" max={10} min={1} id={quality} list="values" name={quality} />
+            </div>
+            <div>
+            <label for={quality + '-improve'} className="py-3  block">Can be improved by: </label>
+            <input className='p-1' type="text" size="38" maxLength={100} id={quality+ '-improve'}/>
+            </div>
         </div>
         )
     });
