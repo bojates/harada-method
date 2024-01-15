1379fbeff42ea3e9d14a28705f387f78d46ca63b
Jemima Kingsley <jemima@bojates.com>
2024-01-15 15:35:29 +0000

    Move to typescript

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,12 +2,23 @@ import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
 
 const handler = NextAuth({
-    providers: [
-        GitHubProvider({
-            clientId: process.env.AUTH_GITHUB_ID ?? "", 
-            clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
-        }),
-    ],
-})
-
+        providers: [
+            GitHubProvider({
+                clientId: process.env.AUTH_GITHUB_ID ?? "", 
+                clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
+            }),
+        ],
+    })
+    
 export { handler as GET, handler as POST }
+
+// export const authOptions = {
+//     providers: [
+//         GitHubProvider({
+//             clientId: process.env.AUTH_GITHUB_ID ?? "", 
+//             clientSecret: process.env.AUTH_GITHUB_SECRET ?? "",
+//         }),
+//     ],
+// };
+// export const handler = NextAuth(authOptions);
+// export { handler as GET, handler as POST }
