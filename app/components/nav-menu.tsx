0473e211a99cb9abc25da424c44c03bf51443ed3
Jemima Kingsley <jemima@bojates.com>
2024-01-16 14:52:05 +0000

    Refactor navigation to array

--- app/components/nav-menu.tsx
+++ app/components/nav-menu.tsx
@@ -1,13 +1,17 @@
 "use client";
 import Link from "next/link";
-// import { signIn, signOut, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 
 const ACTIVE_ROUTE = "py-1 px-2 text-gray-300 bg-gray-700";
 const INACTIVE_ROUTE = "py-1 px-2 text-gray-500 hover:text-gray-300 hover:bg-gray-700";
 
+const NAV = [
+  {'name': 'Home', 
+  'path': '/'} , 
+  { 'name': 'Qualities', 
+  'path': '/qualities'}
+] 
 function AuthButton() {
-    // const { data: session } = useSession();
 
     // if (session) {
     //     return (
@@ -30,61 +34,19 @@ function AuthButton() {
 
 export default function NavMenu() {
     const pathname = usePathname();
+    const links =  NAV.map(navItem => {
+      return (
+        <Link href="{navItem.path}">
+          <li key="{navItem.name}"  className={pathname === navItem.path ? ACTIVE_ROUTE : INACTIVE_ROUTE}>{navItem.name}</li>
+        </Link>
+      );
+    });
   return (
     <div>
       <AuthButton />
       <hr className="my-4" />
       <ul>
-        <Link href="/">
-          <li className={pathname === "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
-            Home
-          </li>
-        </Link>
-        <Link href="/goals/qualities">
-          <li
-            className={
-              pathname === "/goals/qualities" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            Qualities
-          </li>
-        </Link>
-        <Link href="/protected">
-          <li
-            className={
-              pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            Protected Route
-          </li>
-        </Link>
-        <Link href="/serverAction">
-          <li
-            className={
-              pathname === "/serverAction" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            Server Action
-          </li>
-        </Link>
-        <Link href="/apiFromClient">
-          <li
-            className={
-              pathname === "/apiFromClient" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            API From Client
-          </li>
-        </Link>
-        <Link href="/apiFromServer">
-          <li
-            className={
-              pathname === "/apiFromServer" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            API From Server
-          </li>
-        </Link>
+        {links}
       </ul>
     </div>
   );
--- app/components/side-nav.tsx
+++ app/components/side-nav.tsx
@@ -1,22 +1,25 @@
 import NavMenu from "./nav-menu";
 import { signOut } from "@/auth";
-// import { signOut } from "next-auth/react";
 
-export default function SideNav() {
+export default function SideNav({ session }: { session: Session | null }) {
+    const logOutForm = (<form
+    action={async () => {
+      'use server';
+      await signOut();
+    }}
+  >
+    <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+      {/* <PowerIcon className="w-6" /> */}
+      <div className="hidden md:block">Sign Out</div>
+    </button>
+  </form>);
+    
+    const logIn = 'Log In'
     return (
         <>
         <NavMenu />
-        <form
-          action={async () => {
-            'use server';
-            await signOut();
-          }}
-        >
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            {/* <PowerIcon className="w-6" /> */}
-            <div className="hidden md:block">Sign Out</div>
-          </button>
-        </form>
+        { (session?.user) && logOutForm }
+        { (!session?.user) && logIn }
         </>
     )
 }
